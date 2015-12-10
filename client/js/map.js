7e7a11597a34c455072eb1fab3a17357f25c5660
fabienoger <fab-oger@live.fr>
2015-12-10 15:40:04 +0100

    publish package & setting icon

--- client/js/map.js
+++ client/js/map.js
@@ -10,8 +10,8 @@ if (Meteor.isClient) {
         iconUrl: 'castle.png',
 
         iconSize:     [38, 40], // size of the icon
-        iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-        popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+        iconAnchor:   [0, 0], // point of the icon which will correspond to marker's location 22, 94
+        popupAnchor:  [15, 3] // -76 point from which the popup should open relative to the iconAnchor
       });
 
       // Display defaults values
@@ -23,6 +23,7 @@ if (Meteor.isClient) {
       var map = L.map('map').setView(defaultCoordinate, defaultZoom);
 
       L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          minZoom: 2,
           maxZoom: 18,
           attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       }).addTo(map);
@@ -85,28 +86,26 @@ if (Meteor.isClient) {
         // Castle Name
         if (castle.properties.nom != null)
           popupContent += "<h5 class='center-align'>" + castle.properties.nom + "</h5>";
-        // Check if wiki isn't empty
+        // Add wiki to popup
         var wiki = castle.properties.wiki;
         var wikiLink = "";
+        var wikiName = null;
         if (wiki != null) {
           wiki = wiki.split(":");
-          var wikiName = wiki[1].replace(/ /g, "_");
+          wikiName = wiki[1].replace(/ /g, "_");
           wikiLink = "https://" + wiki[0] + ".wikipedia.org/wiki/" + wikiName;
-          popupContent += "<div class='valign-wrapper'><i class='material-icons'>language</i><a class='valign right-align' target='_blank' href=" + wikiLink + ">" + wiki[1] + "</a></div>";
+          wikiName = wiki[1];
         }
+        popupContent += "<div class='valign-wrapper'><i class='material-icons'>language</i><a class='valign right-align' target='_blank' href=" + wikiLink + ">" + wikiName + "</a></div>";
 
-        // Check if adress isn't empty
-        if (castle.properties.adresse != null) {
-          popupContent += "<div class='valign-wrapper'><p class='valign'><i class='material-icons'>room</i>" + castle.properties.adresse + "</p></div>";
-          console.log(castle.properties.adresse);
-        }
+        // Add type to popup
+        popupContent += "<div class='valign-wrapper'><i class='material-icons'>class</i><p class='valign'>" + castle.properties.type + "</p></div>";
 
-        // Check if web site isn't empty
-        if (castle.properties.web_site != null)
-        {
-          popupContent += "<div class='valign-wrapper'><p class='valign'><i class='material-icons'>room</i>" + castle.properties.web_site + "</p></div>";
-          console.log(castle.properties.web_site);
-        }
+        // Add adress to popup
+        popupContent += "<div class='valign-wrapper'><i class='material-icons'>room</i><p class='valign'>" + castle.properties.adresse + "</p></div>";
+
+        // Add web site to popup
+        popupContent += "<div class='valign-wrapper'><i class='material-icons'>web</i><p class='valign'>" + castle.properties.web_site + "</p></div>";
 
         // If popupContent isn't empty add popup to marker
         if (popupContent != "")
--- packages/leaflet-1/package.js
+++ packages/leaflet-1/package.js
@@ -1,19 +1,19 @@
 Package.describe({
-  name: 'foger:leaflet-1',
+  name: 'fabienoger:leaflet-1',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'This package allows to operate the markers clusters leaflet',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/fabienoger/openCastleMap',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  documentation: null
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('ecmascript');
-  api.use('bevanhunt:leaflet');
+  api.use('bevanhunt:leaflet@0.3.14');
   api.addFiles('leaflet-1.js', ['client']);
   api.addFiles('leaflet-1.css', ['client']);
 });
