844a93eb39e868044a1404bcf483841cc002d183
fabienoger <fab-oger@live.fr>
2015-12-10 18:31:03 +0100

    settings button and close menu onclick(li)

--- client/js/header.js
+++ client/js/header.js
@@ -2,6 +2,9 @@ if (Meteor.isClient) {
   $(document).ready(function() {
     Template.header.rendered = function () {
       $(".button-collapse").sideNav();
+      $("#mobile-menu li").click(function() {
+        $('.button-collapse').sideNav('hide');
+      });
     }
     Template.header.events({
       'click #show-settings': function() {
