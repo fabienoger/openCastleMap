7ed7bcc491d35cb157459133e23a9dbbe17a45ef
fabienoger <fab-oger@live.fr>
2015-12-11 16:05:00 +0100

    settings div is 15px to right

--- client/js/header.js
+++ client/js/header.js
@@ -17,10 +17,10 @@ if (Meteor.isClient) {
     }
     Template.header.events({
       'click #show-settings': function() {
-        if ($("#settings").css("right") == "30px")
+        if ($("#settings").css("right") == "15px")
           $("#settings").animate({right: "-260px"});
         else
-          $("#settings").animate({right: "30px"});
+          $("#settings").animate({right: "15px"});
       }
     });
   });
